f136597a3665285248c05a414330de7199843907
kyczawon <lkn16@ic.ac.uk>
2019-11-25 11:09:03 +0000

    fix nao files

---
+++ nao_scripts/scripts/nao2.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+import rospkg
+
+
+import argparse
+import math
+import time
+from naoqi import ALProxy
+import motion
+import sys
+sys.dont_write_bytecode = True
+
+def hello(tts, motionProxy, postureProxy):
+
+    postureProxy.goToPosture("StandInit",0.5)
+
+    # Wake up robot
+    motionProxy.wakeUp()
+    
+    # Joint matrix for the hello movement
+    JointNamesL = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand"]
+    # waving right
+    Arm1 = [-70,  40, 0, -35, 20, 50]
+    Arm1 = [ x * motion.TO_RAD for x in Arm1]
+
+    # waving left
+    Arm2 = [-70,  40, 0, -75, -20, 50]
+    Arm2 = [ x * motion.TO_RAD for x in Arm2]
+    tts.post.say("Hello, do you want a beer?")
+    
+    pFractionMaxSpeed = 0.35
+    
+    motionProxy.angleInterpolationWithSpeed(JointNamesL, Arm1, pFractionMaxSpeed)
+    motionProxy.angleInterpolationWithSpeed(JointNamesL, Arm2, pFractionMaxSpeed)
+    motionProxy.angleInterpolationWithSpeed(JointNamesL, Arm1, pFractionMaxSpeed)
+    motionProxy.angleInterpolationWithSpeed(JointNamesL, Arm2, pFractionMaxSpeed)
+    motionProxy.angleInterpolationWithSpeed(JointNamesL, Arm1, pFractionMaxSpeed)
+    
+def getID(tts, motionProxy, postureProxy):
+    
+    # Joint matrix for the ID movement
+    ID = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand","RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "RHand", "HeadYaw", "HeadPitch"]
+    
+    # pointing at the ID reader
+    ID_position = [30,  -20, 0, -60, 0, 50,70,-50, 0, 0, 50, 50,-50,10]
+    ID_position = [ x * motion.TO_RAD for x in ID_position]
+    
+    # looking at the customer
+    ID_position2 = [30,  -20, 0, -60, 0, 50,70,-50, 0, 0, 50, 50,0,-20]
+    ID_position2 = [ x * motion.TO_RAD for x in ID_position2]
+    
+    pFractionMaxSpeed = 0.3
+    
+    tts.post.say("Please put your ID here, ")
+    
+    motionProxy.post.angleInterpolationWithSpeed(ID, ID_position, pFractionMaxSpeed)
+    
+    tts.post.say("So I can verify that you are over 18!")
+    
+    motionProxy.angleInterpolationWithSpeed(ID, ID_position2, pFractionMaxSpeed)
+    
+    
+def drink_select(tts, motionProxy, postureProxy):
+    # Joint matrix for the touchpad movement
+    Touchpad = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand","RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "RHand", "HeadYaw", "HeadPitch"]
+    
+    # Pointing to the touchpad
+    Touchpad_position = [70,  50, 0, 0, -50, 50, 30, 20, 0, 60, 0, 50, 50,10]
+    Touchpad_position = [ x * motion.TO_RAD for x in Touchpad_position]
+    
+    # tilting its head
+    Touchpad_position2 = [70,  50, 0, 0, -50, 50, 30, 0, 80, 20, 50, 50, 0,0]
+    Touchpad_position2 = [ x * motion.TO_RAD for x in Touchpad_position2]
+    
+    # Opening other arm to list the beers
+    Touchpad_position3 = [70,  50, 0, 0, -50, 50, 30, 0, 80, 20, 50, 50, -20,-20]
+    Touchpad_position3 = [ x * motion.TO_RAD for x in Touchpad_position3]
+    
+    # tilting its head
+    Touchpad_position4 = [70,  50, 0, 0, -50, 50, 30, 0, 80, 20, 50, 50, 20,-20]
+    Touchpad_position4 = [ x * motion.TO_RAD for x in Touchpad_position4]
+    
+    pFractionMaxSpeed = 0.3
+    pFractionMaxSpeed2 = 0.15
+   
+    tts.post.say("Please select the one you want on the touch pad")
+    
+    motionProxy.angleInterpolationWithSpeed(Touchpad, Touchpad_position, pFractionMaxSpeed)
+    time.sleep(1.5)
+    
+    tts.post.say("We have a choice of Guiness, Corona,")
+    
+    motionProxy.post.angleInterpolationWithSpeed(Touchpad, Touchpad_position2, pFractionMaxSpeed2)
+    motionProxy.angleInterpolationWithSpeed(Touchpad, Touchpad_position3, pFractionMaxSpeed2)
+    motionProxy.angleInterpolationWithSpeed(Touchpad, Touchpad_position4, pFractionMaxSpeed2)
+    
+    tts.post.say("Kopperberg and an non alcoholic beer!")
+    
+    motionProxy.angleInterpolationWithSpeed(Touchpad, Touchpad_position3, pFractionMaxSpeed2)
+
+def dance(tts, motionProxy, postureProxy):
+    # Stand to start the dance with the correct balance/posture
+    postureProxy.goToPosture("StandInit", 0.5)
+    
+    tts.post.say("While you are waiting for your beer, look at my dance skills!")
+    
+    time.sleep(2.0)
+    # chacha moves
+    footStepsList = []
+
+    # 1) Step forward with your left foot
+    footStepsList.append([["LLeg"], [[0.06, 0.1, 0.0]]])
+
+    # 2) Sidestep to the left with your left foot
+    footStepsList.append([["LLeg"], [[0.00, 0.16, 0.0]]])
+
+    # 3) Move your right foot to your left foot
+    footStepsList.append([["RLeg"], [[0.00, -0.1, 0.0]]])
+
+    # 4) Sidestep to the left with your left foot
+    footStepsList.append([["LLeg"], [[0.00, 0.16, 0.0]]])
+
+    # 5) Step backward & left with your right foot
+    footStepsList.append([["RLeg"], [[-0.04, -0.1, 0.0]]])
+
+    # 6)Step forward & right with your right foot
+    footStepsList.append([["RLeg"], [[0.00, -0.16, 0.0]]])
+
+    # 7) Move your left foot to your right foot
+    footStepsList.append([["LLeg"], [[0.00, 0.1, 0.0]]])
+
+    # 8) Sidestep to the right with your right foot
+    footStepsList.append([["RLeg"], [[0.00, -0.16, 0.0]]])
+
+    ###############################
+    # Send Foot step
+    ###############################
+    stepFrequency = 0.8
+    clearExisting = False
+    nbStepDance = 2 # defined the number of cycle to make
+
+    for j in range( nbStepDance ):
+        for i in range( len(footStepsList) ):
+            motionProxy.setFootStepsWithSpeed(
+                footStepsList[i][0],
+                footStepsList[i][1],
+                [stepFrequency],
+                clearExisting)
+
+
+
+def joke(tts, motionProxy, postureProxy):
+
+    tts.post.say("To beer or not to beer, that is the question.")
+    tts.post.say("Do you know who wrote this?")
+    time.sleep(1.0)
+    tts.post.say("Shake sBeer ")
+
+def joke1(tts, motionProxy, postureProxy):
+    
+    # Initial Position
+    postureProxy.goToPosture("StandInit",1)
+    time.sleep(1.0)
+    # Joint Matrix
+    JokeL = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand"]
+    JokeR = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "RHand"]
+    JokeH = ["HeadYaw", "HeadPitch"]
+    # Joke telling successive positions
+    Joke_position1L = [45, 30, -90, 0, -90, 50]
+    Joke_position1L = [ x * motion.TO_RAD for x in Joke_position1L]
+    
+    Joke_position2L = [70, -18, -20, -40, -90, -50]
+    Joke_position2L = [ x * motion.TO_RAD for x in Joke_position2L]
+    
+    Joke_position1R = [45, -30, 90, 0, 90, 50]
+    Joke_position1R = [ x * motion.TO_RAD for x in Joke_position1R]
+    
+    Joke_position2R = [70, 18, 20, 40, 90, -50]
+    Joke_position2R = [ x * motion.TO_RAD for x in Joke_position2R]
+    
+    pFractionMaxSpeed = 0.3
+
+    Joke_position1H = [0,-20]
+    Joke_position1H = [ x * motion.TO_RAD for x in Joke_position1H]
+
+    Joke_position2H = [0,20]
+    Joke_position2H = [ x * motion.TO_RAD for x in Joke_position2H]
+    
+    tts.post.say("The past,")
+    motionProxy.angleInterpolationWithSpeed(JokeL, Joke_position1L, pFractionMaxSpeed)
+    
+    tts.post.say("the present ")
+    motionProxy.angleInterpolationWithSpeed(JokeR, Joke_position1R, pFractionMaxSpeed)
+    
+    tts.post.say("and the future walk into bar.")
+    motionProxy.angleInterpolationWithSpeed(JokeH, Joke_position1H, pFractionMaxSpeed)
+    
+    time.sleep(1.5)
+    tts.post.say("It was tense.")
+    pFractionMaxSpeed1 = 0.4
+    motionProxy.post.angleInterpolationWithSpeed(JokeL, Joke_position2L, pFractionMaxSpeed1)
+    motionProxy.post.angleInterpolationWithSpeed(JokeR, Joke_position2R, pFractionMaxSpeed1)
+    motionProxy.post.angleInterpolationWithSpeed(JokeH, Joke_position2H, pFractionMaxSpeed1)
+
+def joke2(tts, motionProxy, postureProxy):
+    
+    # Joint Matrix
+    Joke2L = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand"]
+    Joke2R = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "RHand"]
+    Joke2H = ["HeadYaw", "HeadPitch"]
+    
+    # Joke telling successive positions
+    Joke2_position1L = [90, 40, 0, -90, 50, -50]
+    Joke2_position1L = [ x * motion.TO_RAD for x in Joke2_position1L]
+    
+    Joke2_position2L = [60, 40, -70, -50, -50, 50]
+    Joke2_position2L = [ x * motion.TO_RAD for x in Joke2_position2L]
+    
+    Joke2_position1R = [90, -40, 0, 90, -50, -50]
+    Joke2_position1R = [ x * motion.TO_RAD for x in Joke2_position1R]
+    
+    Joke2_position2R = [10, 0, 0, 0, 0, 50]
+    Joke2_position2R = [ x * motion.TO_RAD for x in Joke2_position2R]
+    
+    Joke2_position3R = [10, 18, 0, 80, 0, 50]
+    Joke2_position3R = [ x * motion.TO_RAD for x in Joke2_position3R]
+    
+    Joke2_position4R = [10, 0, 0, 0, 100, 50]
+    Joke2_position4R = [ x * motion.TO_RAD for x in Joke2_position4R]
+    
+    Joke2_position1H = [0,-20]
+    Joke2_position1H = [ x * motion.TO_RAD for x in Joke2_position1H]
+
+    Joke2_position2H = [10,-20]
+    Joke2_position2H = [ x * motion.TO_RAD for x in Joke2_position2H]
+    
+    pFractionMaxSpeed = 0.15
+    pFractionMaxSpeed1 = 0.25
+    pFractionMaxSpeed2 = 0.4
+    
+    motionProxy.post.angleInterpolationWithSpeed(Joke2L, Joke2_position1L, pFractionMaxSpeed)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2R, Joke2_position1R, pFractionMaxSpeed)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2H, Joke2_position1H, pFractionMaxSpeed)
+    tts.post.say("A neutron walks into a bar.")
+    
+    time.sleep(2.0)
+    tts.post.say("How much for a drink?, the neutron says,")
+    motionProxy.post.angleInterpolationWithSpeed(Joke2L, Joke2_position2L, pFractionMaxSpeed1)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2H, Joke2_position2H, pFractionMaxSpeed1)
+    
+    time.sleep(3.0)
+    tts.post.say("The barman answers: For you? no charge")
+    motionProxy.post.angleInterpolationWithSpeed(Joke2L, Joke2_position1L, pFractionMaxSpeed1)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2R, Joke2_position2R, pFractionMaxSpeed1)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2R, Joke2_position3R, pFractionMaxSpeed2)
+    motionProxy.post.angleInterpolationWithSpeed(Joke2R, Joke2_position2R, pFractionMaxSpeed1)
+    
+    time.sleep(3.0)
+    tts.post.say("Get it?")
+    motionProxy.angleInterpolationWithSpeed(Joke2R, Joke2_position4R, pFractionMaxSpeed1)
+    
+    
+
+    
+def main(ip, port):
+    
+    robotIP = "192.168.43.70"
+    PORT = 9559
+    tts    = ALProxy("ALTextToSpeech", robotIP, PORT)
+    motionProxy = ALProxy("ALMotion", robotIP, PORT)
+    motionProxy.setStiffnesses("Body", 1.0)
+    postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
+
+    postureProxy.goToPosture("StandInit",0.5)
+    rospy.init_node('NAO',anonymous=True)
+    # Wake up robot
+    motionProxy.wakeUp()
+    hello(tts, motionProxy,postureProxy)
+    
+    #getID(tts, motionProxy, postureProxy)
+    
+    #drink_select(tts, motionProxy, postureProxy)
+    
+    #dance(tts, motionProxy, postureProxy)
+   
+    #joke1(tts, motionProxy, postureProxy)
+    
+    #joke2(tts, motionProxy, postureProxy)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default= "192.168.43.70",
+                        help="Robot ip address")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Robot port number")
+
+    args = parser.parse_args()
+    main(args.ip, args.port)
---
+++ nao_scripts/src/nao.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+
+import os
+import rospy
+import rospkg
+import time
+import argparse
+from std_msgs.msg import Bool,String, Float64, UInt8
+from cob_perception_msgs.msg import ColorDepthImageArray as FoundFace
+from naoqi import ALProxy
+import sys
+sys.path.append("/home/human/catkin_ws/src/HCR/nao_scripts/scripts")
+from nao2 import hello,getID,drink_select,dance,joke,joke1,joke2
+
+sys.dont_write_bytecode = True
+
+
+class NAO(object):
+    def __init__(self):
+        self.pub_customer_welcomed = rospy.Publisher("/customerwelcomed",String, queue_size=1)
+        self.pub_to_screen = rospy.Publisher("/term1_displayScreenX",UInt8, queue_size=1)
+        self.pub_to_arm = rospy.Publisher("/pour_drink", UInt8, queue_size=1)
+        self.pub_to_kinect = rospy.Publisher("/tilt_angle",Float64, queue_size=1)
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("--ip", type=str, default= "192.168.43.70",
+                            help="Robot ip address")
+        self.parser.add_argument("--port", type=int, default=9559,
+                            help="Robot port number")
+
+        self.args = self.parser.parse_args()
+        robotIP = "192.168.43.70"
+        PORT = 9559
+        self.tts    = ALProxy("ALTextToSpeech", robotIP, PORT)
+        self.motionProxy = ALProxy("ALMotion", robotIP, PORT)
+        self.motionProxy.setStiffnesses("Body", 1.0)
+        self.postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
+
+        self.postureProxy.goToPosture("StandInit",0.5)
+        # Wake up robot
+        self.motionProxy.wakeUp()
+	self.pub_to_kinect.publish(40)
+
+        self.sub_customer = rospy.Subscriber("/face_detector/face_positions", FoundFace, self.callback_hello, queue_size=1)
+        print("INIT DONE")
+
+    def callback_hello(self,msg):
+        #print("HELLO CALLED")
+        if(len(msg.head_detections) > 0 ):
+            self.sub_customer.unregister() # stop looking for new people
+            hello(self.tts,self.motionProxy,self.postureProxy) # nao says hello
+            #time.sleep(3)
+            getID(self.tts,self.motionProxy,self.postureProxy) # nao asks for ID
+            self.pub_customer_welcomed.publish("Welcomed") # start ID scan
+	    self.sub_age = rospy.Subscriber("/IDimage",String, self.callback_drink_select) # wait until ID confirmed
+
+    def callback_drink_select(self,msg):
+        self.pub_to_kinect.publish(40) # move kinect back up
+        self.sub_age.unregister() # stop looking for age
+        print("Drink Select Called")
+        self.pub_to_screen.publish(1) # select drink screen
+        drink_select(self.tts,self.motionProxy,self.postureProxy) # move Nao to point at touchpad
+        print("Drink Select Done")
+        self.sub_selected = rospy.Subscriber("/term1_buttonPressed",UInt8, self.callback_entertain)
+
+    def callback_entertain(self,msg):
+        self.sub_selected.unregister() # ignore new button pressed
+        self.pub_to_arm.publish(msg) # tell arm to start pouring
+        self.sub_ready = rospy.Subscriber("/drink_poured", Bool, self.callback_drink_ready) # wait until arm is done
+        self.pub_to_screen.publish(2)
+        print("Entertain Called")
+        #dance(self.tts, self.motionProxy, self.postureProxy)
+        #time.sleep(3)
+        #joke(self.tts, self.motionProxy, self.postureProxy)
+        #time.sleep(3)
+        #joke1(self.tts, self.motionProxy, self.postureProxy)
+        #time.sleep(3)
+        joke2(self.tts, self.motionProxy, self.postureProxy)
+        print("Entertain Done")
+
+    def callback_drink_ready(self,msg):
+        self.sub_ready.unregister() # ignore other messages from arm
+        self.pub_to_screen.publish(4) # enjoy responsibly
+        time.sleep(3)
+        print("Drink Ready please collect function called")
+        self.pub_to_screen.publish(3) # other drink?
+        self.sub_selected = rospy.Subscriber("/term1_buttonPressed",UInt8, self.callback_anotherDrink)
+
+    def callback_anotherDrink(self,msg):
+        print("customer responded")
+        print(msg)
+        self.sub_selected.unregister()
+        if(msg.data==3):
+            # other drink selected, go back to selection screen
+            self.callback_drink_select("")
+        else:
+            # restart process
+            self.pub_to_screen.publish(4) #change later on with goodbye screen
+            time.sleep(3) # wait to avoid detecting same customer
+            self.sub_customer = rospy.Subscriber("/face_detector/face_positions", FoundFace, self.callback_hello, queue_size=1)
+
+if __name__ == "__main__":
+    
+    try:
+        rospy.init_node("NAO",  argv=sys.argv)
+        sys.argv = rospy.myargv(argv=sys.argv)
+        nao = NAO()
+        rospy.spin()
+    except rospy.exceptions.ROSInterruptException:
+        print("See ya")
+    except rospy.ROSException as e:
+        if str(e) == "publish() to a closed topic":
+            print("See ya")
+        else:
+            raise e
+    except KeyboardInterrupt:
+        print("Shutting down")
+    print()  # print new line
